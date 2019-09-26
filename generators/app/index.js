1d643e9d69b3762eb425dea5f987808c6312942f
Abhishek Deb <abhishekdeb91@gmail.com>
2019-09-26 17:27:59 +0530

    package fix

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,97 +3,92 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
-const _s = require('underscore.string'),
-    path = require('path'),
-    mkdirp = require('mkdirp');
+const _s = require('underscore.string');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
-    prompting () {
-        // have Yeoman greet the user.
-        this.log(
-            yosay(`Welcome to the Rapid Rest API Generator ${chalk.red('generator-restgoose2')}!`)
-        );
+  prompting () {
+    // have Yeoman greet the user.
+    this.log(
+      yosay(`Welcome to the Rapid Rest API Generator ${chalk.red('generator-restgoose2')}!`)
+    );
 
-        const prompts = [{
-                name: 'dbName',
-                message: 'Database Name',
-                default: 'myDb'
-            },
-            {
-                name: 'dbHost',
-                message: 'Database Host',
-                default: 'localhost'
-            },
-            {
-                name: 'dbPort',
-                message: 'Database Port',
-                default: 27017
-            },
-            {
-                name: 'dbUser',
-                message: 'Database User',
-                default: ''
-            },
-            {
-                type: 'password',
-                name: 'dbPassword',
-                message: 'Database Password',
-                default: ''
-            }
-        ];
+    const prompts = [{
+      name: 'dbName',
+      message: 'Database Name',
+      default: 'myDb'
+    },
+    {
+      name: 'dbHost',
+      message: 'Database Host',
+      default: 'localhost'
+    },
+    {
+      name: 'dbPort',
+      message: 'Database Port',
+      default: 27017
+    },
+    {
+      name: 'dbUser',
+      message: 'Database User',
+      default: ''
+    },
+    {
+      type: 'password',
+      name: 'dbPassword',
+      message: 'Database Password',
+      default: ''
+    }
+    ];
 
-        return this.prompt(prompts).then(props => {
-            // to access props later use this.props.someAnswer;
-            this.props = props;
-            this.props.slugName = _s.slugify(this.appname);
-            this.props.capName = _s.capitalize(this.appname);
-        });
+    return this.prompt(prompts).then(props => {
+      // to access props later use this.props.someAnswer;
+      this.props = props;
+      this.props.slugName = _s.slugify(this.appname);
+      this.props.capName = _s.capitalize(this.appname);
+    });
+  }
+
+  configuring () {
+    const templatesToCopy = ['package.json', 'README.md', 'app.js', 'config'];
+
+    for (let i = 0; i < templatesToCopy.length; i += 1) {
+      this.fs.copyTpl(
+        this.templatePath(templatesToCopy[i]),
+        this.destinationPath(templatesToCopy[i]), this.props
+      );
     }
 
-    configuring () {
-        let templatesToCopy = ['package.json', 'README.md', 'app.js', 'config'];
+    this.fs.copyTpl(
+      this.templatePath('.localrcjs'),
+      this.destinationPath('.localrc'), this.model
+    );
 
-        for (let i = 0; i < templatesToCopy.length; i += 1) {
-            this.fs.copyTpl(
-                this.templatePath(templatesToCopy[i]),
-                this.destinationPath(templatesToCopy[i]), this.props
-            );
-        }
+    this.spawnCommand('git', ['init']);
+  }
 
-        this.fs.copyTpl(
-            this.templatePath('.localrcjs'),
-            this.destinationPath('.localrc'), this.model
-        );
+  writing () {
+    const dirsToCreate = [
+      'api', 'apiObjects', 'models', 'tests', 'docs', 'logs', 'public', 'sdk/unity', 'sdk/angular', 'tmp'
+    ];
 
-        this.spawnCommand('git', ['init']);
+    const filesToCopy = ['.editorconfig', '.gitignore', '.jshintrc', 'eslintrc.json', 'routes', 'middleware', 'sdk'];
+
+    dirsToCreate.forEach(x => {
+      mkdirp(x);
+    });
+
+    for (let i = 0; i < filesToCopy.length; i += 1) {
+      this.fs.copyTpl(
+        this.templatePath(filesToCopy[i]),
+        this.destinationPath(filesToCopy[i]), this.props
+      );
     }
+  }
 
-    writing () {
-
-        let dirsToCreate = [
-            'api', 'apiObjects', 'models', 'tests', 'docs', 'logs', 'public', 'sdk/unity', 'sdk/angular', 'tmp'
-        ];
-
-        let filesToCopy = ['.editorconfig', '.gitignore', '.jshintrc', 'eslintrc.json', 'routes', 'middleware', 'sdk'];
-
-
-        dirsToCreate.forEach(x => {
-            mkdirp(x);
-        });
-
-        for (let i = 0; i < filesToCopy.length; i += 1) {
-            this.fs.copyTpl(
-                this.templatePath(filesToCopy[i]),
-                this.destinationPath(filesToCopy[i]), this.props
-            );
-        }
-
-    }
-
-    install () {
-        this.installDependencies({
-            bower: false
-        });
-
-    }
+  install () {
+    this.installDependencies({
+      bower: false
+    });
+  }
 };
